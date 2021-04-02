4787eebf0242a32cf93427af225105933a48c064
Samuel Fox <samueljasonfox@gmail.com>
2021-04-02 10:39:05 -0700

    setting up under-construction homepage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <h1>TrakMyProject</h1>
+        <p>Keep others up to date with <span className="emphasize">what</span> your building and <span className="emphasize">how</span> you're building it.</p>
+        <br />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Circle-icons-roadblock.svg/600px-Circle-icons-roadblock.svg.png" className="App-logo" alt="under construction" />
+        <p>Site Under Construction</p>
+        <p>Follow along! <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/samuelfox1/trakmyproject"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          github.com/samuelfox1/trakmyproject
         </a>
+        </p>
+        <h6>A project by <a className="App-link" href="https://samueljfox.com" target="_blank" rel="noopener noreferrer"
+        >samueljfox.com</a></h6>
       </header>
-    </div>
+    </div >
   );
 }
 
