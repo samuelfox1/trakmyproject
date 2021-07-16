b17f6179df2d8d9ee806f22c5ce425362269dcc0
Samuel Fox <samueljasonfox@gmail.com>
2021-07-16 06:21:14 -0700

    added username & email to UserInfo component

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,19 +1,21 @@
 import React, { useContext } from 'react'
 import { UserContext } from '../../context/UserContext'
-import { Flex, H5, Image } from '../Elements/Elements'
+import { A, Flex, H3, H5, Image } from '../Elements/Elements'
 import './UserInfo.css'
 
 export default function UserInfo() {
     const { loggedInUser } = useContext(UserContext)
-    const { dateCreated } = loggedInUser
-    const { profilePic } = loggedInUser
+    const { dateCreated, profilePic, username, email } = loggedInUser
 
-    console.log(loggedInUser)
+    // console.log(loggedInUser)
     return (
         <>
             <Flex className='user-info-container'>
                 <Image className='profile-pic' src={profilePic} alt='profile' />
-                <H5>user since: {new Date(dateCreated).getFullYear()}</H5>
+                <H3 className='reset'>{username}</H3>
+                <A className='reset' href={email}>{email}</A>
+                <br />
+                <H5 className='reset'>user since: {new Date(dateCreated).getFullYear()}</H5>
             </Flex>
         </>
     )
