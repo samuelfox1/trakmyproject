a16dde3ed31a9fe7b990ccc3c59601efc45b6ba8
Samuel Fox <samueljasonfox@gmail.com>
2021-07-16 06:10:15 -0700

    added userProjectsContext component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
 import { UserContext } from "./context/UserContext";
+import { UserProjectsContext } from "./context/UserProjectsContext";
 import Nav from "./components/Nav/Nav";
 import Landing from "./pages/Landing";
 import SignUp from "./pages/SignUp";
@@ -12,42 +13,45 @@ import { Flex } from "./components/Elements/Elements";
 
 function App() {
   const [loggedInUser, setLoggedInUser] = useState({ loggedIn: false })
+  const [userProjects, setUserProjects] = useState([])
 
 
   return (
     <Router>
       <UserContext.Provider value={{ loggedInUser, setLoggedInUser }}>
+        <UserProjectsContext.Provider value={{ userProjects, setUserProjects }}>
 
-        <Nav />
-        <Flex className='App'>
+          <Nav />
+          <Flex className='App'>
 
-          <Route exact path='/'>
-            {
-              loggedInUser.loggedIn
-                ? <Redirect to={`/user/${loggedInUser.username}`} />
-                : <Landing />
-            }
-          </Route>
+            <Route exact path='/'>
+              {
+                loggedInUser.loggedIn
+                  ? <Redirect to={`/user/${loggedInUser.username}`} />
+                  : <Landing />
+              }
+            </Route>
 
-          <Route exact path='/signup'>
-            {
-              loggedInUser.loggedIn
-                ? <Redirect to={`/user/${loggedInUser.username}`} />
-                : <SignUp />
-            }
-          </Route>
+            <Route exact path='/signup'>
+              {
+                loggedInUser.loggedIn
+                  ? <Redirect to={`/user/${loggedInUser.username}`} />
+                  : <SignUp />
+              }
+            </Route>
 
-          <Route exact path='/user/:username'>
-            {
-              !loggedInUser.loggedIn
-                ? <Redirect to='/' />
-                : <Home />
-            }
-          </Route>
+            <Route exact path='/user/:username'>
+              {
+                !loggedInUser.loggedIn
+                  ? <Redirect to='/' />
+                  : <Home />
+              }
+            </Route>
 
-        </Flex>
-        <Footer />
+          </Flex>
+          <Footer />
 
+        </UserProjectsContext.Provider>
       </UserContext.Provider>
     </Router >
   )
--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,4 +1,4 @@
 import { createContext } from 'react'
 
-export const UserContext = createContext({ loggedInUser: { loggedIn: false } })
-UserContext.displayName = 'UserData'
+// export const UserContext = createContext({ loggedInUser: { loggedIn: false } })
+export const UserContext = createContext()
