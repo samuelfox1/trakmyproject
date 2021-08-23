7ac5f1e52fe092d190c0968df3313d41c529ddcd
Samuel Fox <samueljasonfox@gmail.com>
2021-08-23 03:34:21 -0700

    add: return to home button on page not found component

--- src/pages/PageNotFound/PageNotFound.js
+++ src/pages/PageNotFound/PageNotFound.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { H1 } from '../../components/Elements/Elements'
+import { Link } from 'react-router-dom'
+import { Button, H1 } from '../../components/Elements/Elements'
 import Modal from '../../components/Modal/Modal'
 import './PageNotFound.css'
 
@@ -7,6 +8,7 @@ export default function PageNotFound() {
     return (
         <Modal className="display">
             <H1 className="page-not-found">Page Not Found</H1>
+            <Link to='/'><Button>Home</Button></Link>
         </Modal>
     )
 }
