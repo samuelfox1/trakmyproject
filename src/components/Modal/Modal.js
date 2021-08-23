48aa3d4d8635e93d4c79e7482bd5b92a928f0f4c
Samuel Fox <samueljasonfox@gmail.com>
2021-08-23 02:05:33 -0700

    add: modal component

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from '../Elements/Elements'
+import './Modal.css'
+
+export default function Modal({ className, children }) {
+
+    return (
+        <>
+            <div id='blur-screen'></div>
+            < div id="myModal" className={`${className ? className : ''} modal`} >
+
+                <div className="modal-content border-red">
+                    {/* <span className="close">&times;</span> */}
+                    {children}
+                    <Link to='/'><Button>Home</Button></Link>
+                </div>
+
+            </div >
+        </>
+    )
+}
