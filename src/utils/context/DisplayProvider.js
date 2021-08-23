6ead413d7d453bf61dff82b652c92ef4e13c992e
Samuel Fox <samueljasonfox@gmail.com>
2021-08-23 01:55:47 -0700

    add: DisplayProvider context provider

---
+++ src/utils/context/DisplayProvider.js
@@ -0,0 +1,27 @@
+import React, { useState, createContext, useContext } from 'react';
+
+export const DisplayContext = createContext()
+
+export const useDisplayContext = () => useContext(DisplayContext)
+
+const DisplayProvider = (props) => {
+    const [display, setDisplay] = useState({
+        login: false,
+        signUp: false,
+        pageNotFound: true,
+    });
+
+    const setLoadingStatus = (loadingKey, boolean) => {
+        localStorage.setItem(loadingKey, boolean)
+    }
+    const getLoadingStatus = (loadingKey) => localStorage.getItem(loadingKey) === 'true' ? true : false
+
+    return (
+        <DisplayContext.Provider
+            value={{ display, setDisplay, getLoadingStatus, setLoadingStatus }}
+            {...props}
+        />
+    );
+};
+
+export default DisplayProvider;
